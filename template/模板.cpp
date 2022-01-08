b928bd196a35569c863f0981e71ef40df19707e8
luakert <ninjass@sina.com>
2022-01-08 17:18:14 +0800

    dd

---
+++ template/模板.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+template<typename T>
+void my_swap(T& a, T& b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template<typename T>
+void sorrt_Arr(T[] arr)
+{
+    for (int i = 0; i < sizeof(arr)/sizeof(T); i++)
+    {
+        int max = i;
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[max] < arr[j])
+            {
+                max = j;
+            }
+        }
+
+        if (max != i)
+        {
+            my_swap(arr[max], arr[i]);
+        }
+    }
+
+}
+
+template<typename T>
+void printA(T[] arr)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << " " << end;
+    }
+}
+
+void test1()
+{
+    int a = 1;
+    int b = 2;
+    char e ='e';
+    double c = 3.0;
+    double d = 4.0;
+    cout << "交换前a =" << a << " b=" << b << endl;
+    my_swap(a, b);
+    cout << "交换后a =" << a << " b=" << b << endl;
+    my_swap<double>(c, d);
+
+    char charArr[] = "ceadf";
+    sorrt_Arr(charArr);
+    printA(charArr);
+}
+
+
+void main()
+{
+
+    test1();
+    system("pause");
+    system("exit");
+}
