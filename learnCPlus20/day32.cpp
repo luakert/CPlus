591384dbe68efd9af09473bb6ba58f83c5a14e29
luakert <ninjass@sina.com>
2024-02-24 00:59:29 +0800

    add new

---
+++ learnCPlus20/day32.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fish;
+class Bear;
+class TRex;
+// Ë«·ÖÅÉ
+class Animal
+{
+public:
+    virtual bool eats(const Animal& prey) const = 0;
+    virtual bool eatenBy(const Bear&) const = 0;
+    virtual bool eatenBy(const Fish&) const = 0;
+    virtual bool eatenBy(const TRex&) const = 0;
+};
+
+class Bear : public Animal
+{
+public:
+    bool eats(const Animal& prey) const override {
+        return prey.eatenBy(*this);
+    }
+
+    bool eatenBy(const Bear&) const override { return false; }
+    bool eatenBy(const Fish&) const override { return false; }
+    bool eatenBy(const TRex&) const override { return true; }
+};
+
+class Fish : public Animal
+{
+public:
+    bool eats(const Animal& prey) const override {
+        return prey.eatenBy(*this);
+    }
+
+    bool eatenBy(const Bear&) const override { return true; }
+    bool eatenBy(const Fish&) const override { return true; }
+    bool eatenBy(const TRex&) const override { return true; }
+};
+
+class TRex : public Animal
+{
+public:
+    bool eats(const Animal& prey) const override {
+        return prey.eatenBy(*this);
+    }
+
+    bool eatenBy(const Bear&) const override { return false; }
+    bool eatenBy(const Fish&) const override { return false; }
+    bool eatenBy(const TRex&) const override { return true; }
+};
+int main()
+{
+    cout << "hello world" << endl;
+    system("pause");
+}
