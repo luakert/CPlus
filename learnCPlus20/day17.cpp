fd37e58d9b4ffa58173a325c695374eeada3fbc2
luakert <ninjass@sina.com>
2024-05-03 01:25:42 +0800

    add iterator type and input&traits

--- learnCPlus20/day17.cpp
+++ learnCPlus20/day17.cpp
@@ -2,6 +2,8 @@
 #include <iterator>
 #include <memory>
 #include <vector>
+#include <format>
+
 
 using namespace std;
 
@@ -13,11 +15,60 @@ void iteratorTraitsTest(IteratorType it)
 	cout << temp << endl;
 }
 
+template<typename Iter>
+void myPrint(Iter begin, Iter end)
+{
+	for (auto iter{ begin }; iter != end; ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+}
+
+template<typename Iter>
+auto myFind(Iter begin, Iter end, const typename iterator_traits<Iter>::value_type& value)
+{
+	for (auto iter{ begin }; iter != end; ++iter)
+	{
+		if (*iter == value)
+		{
+			return iter;
+		}
+	}
+
+	return end;
+}
+
+template<typename InputIter, typename OutputIter>
+void myCopy(InputIter begin, InputIter end, OutputIter target)
+{
+	for (auto iter{begin}; iter != end; ++iter, ++target)
+	{
+		*target = *iter;
+	}
+}
 
 int main()
 {
 	vector v{ 5 };
 	iteratorTraitsTest(cbegin(v));
 
+	vector v2{ 1, 2, 4,5,6,8 };
+	myPrint(cbegin(v2), cend(v2));
+
+	vector v3{ 11, 22, 33 };
+	auto result{ myFind(cbegin(v3), cend(v3), 33) };
+	if (result != cend(v3))
+	{
+		cout << format("Found values at postition {}", distance(cbegin(v3), result));
+	}
+	cout << endl;
+	vector resourceVector{ 11, 23, 43, 564, 233 };
+	vector<int> destVector(resourceVector.size());
+	myCopy(cbegin(resourceVector), cend(resourceVector), begin(destVector));
+	myCopy(cbegin(resourceVector), cend(resourceVector), ostream_iterator<int>{cout, " "});
+	cout << endl;
+	myCopy(cbegin(destVector), cend(destVector), ostream_iterator<int>{cout, " "});
+	cout << endl;
 	system("pause");
 }
