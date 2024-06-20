77123f759d60952630ac752595888edeb0b95913
luakert <ninjass@sina.com>
2024-06-20 23:54:10 +0800

    add day24

--- learnCPlus20/day24.cpp
+++ learnCPlus20/day24.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <variant>
+#include <utility>
 #include <string>
-#
+#include <format>
+#include <any>
 
 using namespace std;
 
@@ -33,7 +35,32 @@ void test2401()
     cout << "float=" << (thefloat ? *thefloat : 0.0) << endl;
     cout << "retrieved string " << (theString ? *theString : "null") << endl;
     cout << "retrieved int " << (theInt ? *theInt : 0) << endl;
+}
 
+void test2402()
+{
+    any empty;
+    any anInt{ 3 };
+    any anString{ "An std::string"s };
+
+    cout << "empty has value " << empty.has_value() << endl;
+    cout << "anInt has value " << anInt.has_value() << endl;
+
+    int theInt{ static_cast<int>(anInt) };
+    cout << theInt << endl;
+
+    try
+    {
+        int test{ static_cast<int>(anString) };
+    }
+    catch (const bad_any_cast& ex)
+    {
+        cout << ex.what() << endl;
+    }
+
+    using MyTuple = tuple<int, string, bool>;
+    MyTuple t1{ 16, "Test"s, false };
+    cout << format("t1 =({}, {}, {})", get<0>(t1), get<1>(t1), get<2>(t1)) << endl;
 
 }
 
